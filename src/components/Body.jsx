6f35b37255146d6f5891b939ec48c07d5354efaf
vunguyenkhanh <vunguyenkhanh.6599@gmail.com>
2024-10-12 09:30:21 +0700

    chore: upload source code from local

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner"
+import Item from "./Item"
+
+const Body = () => {
+  return (
+    <>
+      <Banner />
+      <Item />
+    </>
+  )
+}
+
+export default Body
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <>
+      {/* Footer*/}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">Copyright © Your Website 2023</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
